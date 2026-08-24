63e5b5c4f5a8bd87b1abbba90bbc985caf397274
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-08-24 08:57:03 +0700

    Match settings client

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,8 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
-			emitTsDeclarations: true
+			emitTsDeclarations: true,
+			strategy: ['url', 'baseLocale']
 		})
 	],
 	test: {
